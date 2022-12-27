57466f57e9aefc0fe46f7122c66cde9f21d9503e
Mattias Velamsson <mattiaswelamsson@gmail.com>
2022-12-27 22:25:42 +0900

    fixed bug and typo in CartProvider

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,20 +1,23 @@
-// import CartContext from "./cart-context";
+import CartContext from "./cart-context";
 
 const CartProvider = (props) => {
   const addItemToCartHandler = (item) => {};
   const removeItemFromCartHandler = (id) => {};
 
-  const CartContext = {
+  const cartContext = {
     items: [],
     totalAmount: 0,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
 
-  return <CartContext.Provider>{props.child}</CartContext.Provider>;
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
-
 /// SOMETHING HERE _____ LINE 14?
 /// You've made a component inside a component. Check CartContext -----
