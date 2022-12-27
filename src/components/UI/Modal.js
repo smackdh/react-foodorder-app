a7fcd603be671d384b7a0f9c91e54ca1ef82314d
Mattias Velamsson <mattiaswelamsson@gmail.com>
2022-12-27 14:51:14 +0900

    added overlay div in html dom for portal

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {};
+
+const Modal = () => {
+  return;
+};
+
+export default Modal;
