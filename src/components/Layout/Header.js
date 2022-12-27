2921794827662ab8bda1c7c7dcc9902db337160b
Mattias Velamsson <mattiaswelamsson@gmail.com>
2022-12-27 11:16:41 +0900

    completed button component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import { Fragment } from "react";
 import classes from "./Header.module.css";
 import meals from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = () => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>Husmans</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img className={["main-image.image"]} src={meals} alt="meal table" />
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,8 +1,17 @@
 import React from "react";
 import classes from "./HeaderCartButton.module.css";
+import CartIcon from "./CartIcon";
 
 const HeaderCartButton = () => {
-  return <button>Cart</button>;
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}> 3</span>
+    </button>
+  );
 };
 
 export default HeaderCartButton;
