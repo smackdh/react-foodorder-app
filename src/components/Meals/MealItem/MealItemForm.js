ed25c2e6ec1eee167757e730cf2a05435b66efcf
Mattias Velamsson <mattiaswelamsson@gmail.com>
2022-12-27 14:08:56 +0900

    added input component

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,7 +1,21 @@
 import React from "react";
 import classes from "./MealItemForm.module.css";
-const MealItemForm = () => {
-  return;
+import Input from "./Input";
+
+const MealItemForm = (props) => {
+  const addHandler = (e) => {
+    e.preventDefault();
+    console.log("testing");
+  };
+
+  return (
+    <form className={classes.form}>
+      <Input />
+      <button onClick={addHandler} type="submit">
+        + Add
+      </button>
+    </form>
+  );
 };
 
 export default MealItemForm;
