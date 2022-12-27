1c6bc9634c5020a48fdc4e1f427d75e6f7a2eaee
Mattias Velamsson <mattiaswelamsson@gmail.com>
2022-12-27 11:27:49 +0900

    created files for meals

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MealsSummary = () => {
+  return <div></div>;
+};
+
+export default MealsSummary;
---
+++ src/components/Meals/dummy-meals.js
@@ -0,0 +1,26 @@
+const DUMMY_MEALS = [
+  {
+    id: "m1",
+    name: "Swedish Meatballs",
+    description: "Finest dish Sweden has to offer.",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Bibimbap",
+    description: "A Korean speciality!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Sushi",
+    description: "Healthy...and protein rich!",
+    price: 18.99,
+  },
+];
